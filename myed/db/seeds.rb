a948c17e8ed28f98e98ac38b6ecd4312bd751f44
DIANA JURGIELEWICZ <DMJURG@GMAIL.COM>
2015-04-24 00:08:23 -0400

    CRUD for users and profiles

--- myed/db/seeds.rb
+++ myed/db/seeds.rb
@@ -4,29 +4,34 @@ Category.destroy_all
 
 profiles = [
   {
-    name: "Smokey t. Bear",
+    name: "Smokey the Bear",
     age: 71,
-    occupation: "Fire Protection Engineer"
+    occupation: "Fire Protection Engineer",
+    image: "http://upload.wikimedia.org/wikipedia/commons/6/62/SmokeyTheBearHeadshot.GIF"
   },
   {
-    name: "Michelle O.",
+    name: "Michelle Obama",
     age: 51,
-    occupation: "Government"
+    occupation: "Government",
+    image: "http://myhero.com/images/guest/g266634/hero92090/michelle-obama-thumb.jpg"
   },
   {
     name: "Snoopy",
     age: 21,
-    occupation: "Pilot"
+    occupation: "Pilot",
+    image: "http://www.picgifs.com/clip-art/cartoons/snoopy/clip-art-snoopy-124159.jpg"
   },
   {
-    name: "Frodo B.",
+    name: "Frodo Baggins",
     age: 33,
-    occupation: "Courier"
+    occupation: "Courier",
+    image: "http://fc04.deviantart.net/fs70/f/2014/282/3/6/frodo_baggins_x_reader__games_we_play__au___by_case2000-d80790n.jpg"
   },
   {
-    name: "Lucy R.",
+    name: "Lucy Ricardo",
     age: 37,
-    occupation: "Hijinks"
+    occupation: "Hijinks",
+    image: "http://vignette1.wikia.nocookie.net/p__/images/a/ac/Lucy_Ricardo.png/revision/latest?cb=20150305142341&path-prefix=protagonist"
   }
 ]
 
@@ -63,7 +68,7 @@ stories = [
     body: "Created PSAs to encourage park visitors to take personal responsibility for their actions",
     lesson: "Communication, PR",
     photo_url: "http://www.adlibbing.org/wp-content/uploads/2014/08/smokey-cub.jpg",
-    author: "Smokey t. Bear"
+    author: "Smokey the Bear"
   },
   {
     category: "Other",
@@ -71,7 +76,7 @@ stories = [
     body: "Planted a garden with local kids to encourage healthy eating and encourage collaboration",
     lesson: "Developing community, health",
     photo_url:  "http://www4.pictures.zimbio.com/gi/First+Lady+Michelle+Obama+Holds+Food+Nutrition+IjfGZDCAEUtl.jpg",
-    author: "Michelle O."
+    author: "Michelle Obama"
   },
   {
     category: "Vocational Training",
@@ -87,7 +92,7 @@ stories = [
     body: "Took a journey through a variety of lands both with and encountering a variety of ethnicities",
     lesson: "Race relations, mediation, endurance",
     photo_url: "http://img3.wikia.nocookie.net/__cb20130619032635/lotr/images/c/c5/Frodo_Baggins_and_Sam_Travel.png",
-    author: "Frodo B."
+    author: "Frodo Baggins"
   },
   {
     category: "Interpersonal Relationships",
@@ -95,7 +100,7 @@ stories = [
     body: "Got the chance to do a miming exercise with Harpo Marx!",
     lesson: "How to be more intuitive",
     photo_url: "https://nypdecider.files.wordpress.com/2014/08/i-love-lucy-lucy-meets-harpo-marx.png?w=642&h=428&crop=1",
-    author: "Lucy R."
+    author: "Lucy Ricardo"
   },
 ]
 
